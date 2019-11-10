a93cf506250c83f5fd6bbcf2f7c8ffc0be6cd772
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-11-10 18:11:14 +0000

    Adds fading animation to links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
+
 import ProfilePicture from "../../assets/img/profile.jpg";
 import "./Header.css";
 
@@ -12,56 +14,80 @@ const Header = ({ page, setPage }) => {
         <nav className="main-nav">
           <ul>
             {/* ----------------------ABOUT----------------------------*/}
-            <li className={page === "about" ? "activeLink" : null}>
-              <button
-                id="aboutTab"
-                className="tab"
-                onClick={() => setPage("about")}
-              >
-                <i className="fas fa-female"></i>
-                <span className="header-title">About Me</span>
-                <span className="header-subtitle">What I do</span>
-              </button>
-            </li>
+            <CSSTransition
+              in={page === "about"}
+              timeout={600}
+              className="link-about"
+            >
+              <li>
+                <button
+                  id="aboutTab"
+                  className="tab"
+                  onClick={() => setPage("about")}
+                >
+                  <i className="fas fa-female"></i>
+                  <span className="header-title">About Me</span>
+                  <span className="header-subtitle">What I do</span>
+                </button>
+              </li>
+            </CSSTransition>
             {/* ----------------------RESUME----------------------------*/}
-            <li className={page === "resume" ? "activeLink" : null}>
-              <button
-                id="resumeTab"
-                className="tab"
-                // style={page === "resume" ? activeLinkStyle : null}
-                onClick={() => setPage("resume")}
-              >
-                <i className="fas fa-suitcase"></i>
-                <span className="header-title">Resume</span>
-                <span className="header-subtitle">What I have Done</span>
-              </button>
-            </li>
+            <CSSTransition
+              in={page === "resume"}
+              timeout={600}
+              className="link-resume"
+            >
+              <li>
+                <button
+                  id="resumeTab"
+                  className="tab"
+                  // style={page === "resume" ? activeLinkStyle : null}
+                  onClick={() => setPage("resume")}
+                >
+                  <i className="fas fa-suitcase"></i>
+                  <span className="header-title">Resume</span>
+                  <span className="header-subtitle">What I have Done</span>
+                </button>
+              </li>
+            </CSSTransition>
             {/* ----------------------PORTFOLIO----------------------------*/}
-            <li className={page === "portfolio" ? "activeLink" : null}>
-              <button
-                id="portfolioTab"
-                className="tab"
-                onClick={() => setPage("portfolio")}
-                // style={page === "portfolio" ? activeLinkStyle : null}
-              >
-                <i className="fas fa-eye"></i>
-                <span className="header-title">Portfolio</span>
-                <span className="header-subtitle">What I can Do</span>
-              </button>
-            </li>
+            <CSSTransition
+              in={page === "portfolio"}
+              timeout={600}
+              className="link-portfolio"
+            >
+              <li>
+                <button
+                  id="portfolioTab"
+                  className="tab"
+                  onClick={() => setPage("portfolio")}
+                  // style={page === "portfolio" ? activeLinkStyle : null}
+                >
+                  <i className="fas fa-eye"></i>
+                  <span className="header-title">Portfolio</span>
+                  <span className="header-subtitle">What I can Do</span>
+                </button>
+              </li>
+            </CSSTransition>
             {/* ----------------------CONTACT----------------------------*/}
-            <li className={page === "contact" ? "activeLink" : null}>
-              <button
-                id="contactTab"
-                className="tab"
-                // style={page === "contact" ? activeLinkStyle : null}
-                onClick={() => setPage("contact")}
-              >
-                <i className="fas fa-envelope"></i>
-                <span className="header-title">Contact</span>
-                <span className="header-subtitle">What you can do</span>
-              </button>
-            </li>
+            <CSSTransition
+              in={page === "contact"}
+              timeout={600}
+              className="link-contact"
+            >
+              <li>
+                <button
+                  id="contactTab"
+                  className="tab"
+                  // style={page === "contact" ? activeLinkStyle : null}
+                  onClick={() => setPage("contact")}
+                >
+                  <i className="fas fa-envelope"></i>
+                  <span className="header-title">Contact</span>
+                  <span className="header-subtitle">What you can do</span>
+                </button>
+              </li>
+            </CSSTransition>
           </ul>
         </nav>
       </div>
